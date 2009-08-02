a8d1e6184bf855a897718b951e90690e5261cabe
SEO Design Solutions <SEO Design Solutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-02 04:38:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/seo-ultimate/trunk@142038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo-ultimate.php
+++ seo-ultimate.php
@@ -3,7 +3,7 @@
 Plugin Name: SEO Ultimate
 Plugin URI: http://www.seodesignsolutions.com/wordpress-seo/
 Description: This all-in-one SEO plugin can rewrite title tags, set meta data, add noindex, insert canonical tags, log 404 errors, edit your robots.txt, and more.
-Version: 0.9.2
+Version: 0.9.3
 Author: SEO Design Solutions
 Author URI: http://www.seodesignsolutions.com/
 Text Domain: seo-ultimate
@@ -12,7 +12,7 @@ Text Domain: seo-ultimate
 /**
  * The main SEO Ultimate plugin file.
  * @package SeoUltimate
- * @version 0.9.2
+ * @version 0.9.3
  * @link http://www.seodesignsolutions.com/wordpress-seo/ SEO Ultimate Homepage
  */
 
@@ -38,10 +38,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //Reading plugin info from constants is faster than trying to parse it from the header above.
 define("SU_PLUGIN_NAME", "SEO Ultimate");
 define("SU_PLUGIN_URI", "http://www.seodesignsolutions.com/wordpress-seo/");
-define("SU_VERSION", "0.9.2");
+define("SU_VERSION", "0.9.3");
 define("SU_AUTHOR", "SEO Design Solutions");
 define("SU_AUTHOR_URI", "http://www.seodesignsolutions.com/");
-define("SU_USER_AGENT", "SeoUltimate/0.9.2");
+define("SU_USER_AGENT", "SeoUltimate/0.9.3");
 
 define('SU_MODULE_ENABLED', 10);
 define('SU_MODULE_SILENCED', 5);
