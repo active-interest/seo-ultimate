182fe911bf2338041f57cfe6511becc814e38622
SEO Design Solutions <SEO Design Solutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-25 22:31:57 +0000

    2010-01-25: Version 1.5.2
    git-svn-id: https://plugins.svn.wordpress.org/seo-ultimate/tags/1.5.2@198110 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/jlfunctions/url.php
@@ -0,0 +1,33 @@
+<?php
+/*
+JLFunctions URL Class
+Copyright (c)2009 John Lamansky
+All rights reserved
+May not be redistributed or used without express written permission.
+*/
+
+class suurl {
+	
+	/**
+	 * Approximately determines the URL in the visitor's address bar. (Includes query strings, but not #anchors.)
+	 * 
+	 * @return string The current URL.
+	 */
+	function current() {
+		$url = 'http';
+		if ($_SERVER["HTTPS"] == "on") $url .= "s";
+		$url .= "://";
+		
+		if ($_SERVER["SERVER_PORT"] != "80")
+			return $url.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		else
+			return $url.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	
+	function build_query($array) {
+		return html_entity_decode(http_build_query($array));
+	}
+
+}
+
+?>
